ff2550f418f9cd7df49b78600867728feb5145e6
Marko Tasic <mtasic85@gmail.com>
2014-06-14 17:45:18 +0200

    containers

---
+++ static/dockyard/js/container.js
@@ -0,0 +1,333 @@
+"use strict";
+
+/*
+ * container
+ */
+var container = {};
+
+$.extend(container, {
+    table: null,
+    
+    list: function(options) {
+        options = options || {};
+        
+        $.ajax({
+            type: 'POST',
+            url: '/containers/all',
+            contentType: 'application/json;charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify({}),
+        })
+        .done(function(data) {
+            // console.log(data);
+            _.each(data.containers, function(container_) {
+                container._add(container_);
+            });
+        })
+        .error(function (xhr, ajaxOptions, thrownError) {
+            $.bootstrapGrowl('Oops, something went wrong!', {type: 'info'});
+        });
+    },
+    
+    _add: function(container_) {
+        var tbody = container.table.find('tbody');
+        var tr_template = _.template($('#table-row-container').html());
+        var edit_template = _.template($('#modal-edit-container').html());
+        
+        var tr = $(tr_template(container_))
+            .appendTo(tbody);
+        
+        // edit
+        tr.find('a#edit').click(function(e) {
+            var modal_div = $(edit_template(container_));
+            var host_id_select = modal_div.find('#host_id');
+            var mount_point_id_select = modal_div.find('#mount_point_id');
+            
+            // populate hosts
+            $.ajax({
+                type: 'POST',
+                url: '/hosts/all',
+                contentType: 'application/json;charset=utf-8',
+                dataType: 'json',
+                data: JSON.stringify({}),
+            })
+            .done(function(data) {
+                // console.log(data);
+                _.each(data.hosts, function(host_) {
+                    var option = $('<option>')
+                        .attr('value', host_.id)
+                        .text(host_.name)
+                        .appendTo(host_id_select);
+                });
+                
+                // select host
+                host_id_select.val(container_.host_id);
+            })
+            .error(function (xhr, ajaxOptions, thrownError) {
+                $.bootstrapGrowl('Oops, something went wrong!', {type: 'info'});
+            });
+            
+            // populate mount points
+            $.ajax({
+                type: 'POST',
+                url: '/mount/points/all',
+                contentType: 'application/json;charset=utf-8',
+                dataType: 'json',
+                data: JSON.stringify({}),
+            })
+            .done(function(data) {
+                // console.log(data);
+                _.each(data.mounts, function(mount_point_) {
+                    var option = $('<option>')
+                        .attr('value', mount_point_.id)
+                        .text(mount_point_.name)
+                        .appendTo(mount_point_id_select);
+                });
+                
+                // select host
+                mount_point_id_select.val(container_.mount_point_id);
+            })
+            .error(function (xhr, ajaxOptions, thrownError) {
+                $.bootstrapGrowl('Oops, something went wrong!', {type: 'info'});
+            });
+            
+            // close
+            modal_div.find('button#close').click(function(e) {
+                // close modal
+                modal_div.modal('hide');
+                setTimeout(function() { modal_div.remove();}, 500);
+                $('.modal-backdrop').remove();
+            });
+            
+            // update
+            modal_div.find('button#update').click(function(e) {
+                // close modal
+                modal_div.modal('hide');
+                setTimeout(function() { modal_div.remove();}, 500);
+                $('.modal-backdrop').remove();
+                
+                // update container
+                var _container = {
+                    id: container_.id,
+                    name: modal_div.find('#name').val(),
+                    capacity: modal_div.find('#capacity').val(),
+                    host_id: modal_div.find('#host_id').val(),
+                    mount_point_id: modal_div.find('#mount_point_id').val(),
+                    username: modal_div.find('#username').val(),
+                };
+                
+                $.ajax({
+                    type: 'POST',
+                    url: '/container/update',
+                    contentType: 'application/json;charset=utf-8',
+                    dataType: 'json',
+                    data: JSON.stringify({
+                        container: _container,
+                    }),
+                })
+                .done(function(data) {
+                    // required to fix variable "container" from closure
+                    _.each(_container, function(value, key) { container_[key] = value; });
+                    
+                    // update UI
+                    container._update(data.container);
+                    $.bootstrapGrowl('Volume successfully updated.', {type: 'success'});
+                })
+                .error(function (xhr, ajaxOptions, thrownError) {
+                    $.bootstrapGrowl('Oops, something went wrong!', {type: 'info'});
+                });
+            });
+            
+            modal_div.modal({
+                backdrop: 'static',
+            });
+        });
+        
+        // activate
+        tr.find('a#activate').click(function(e) {
+            // update container
+            var _container = {
+                id: container_.id,
+                active: true,
+            };
+            
+            $.ajax({
+                type: 'POST',
+                url: '/container/update',
+                contentType: 'application/json;charset=utf-8',
+                dataType: 'json',
+                data: JSON.stringify({
+                    container: _container,
+                }),
+            })
+            .done(function(data) {
+                // required to fix variable "container" from closure
+                _.each(_container, function(value, key) { container_[key] = value; });
+                
+                // update UI
+                container._update(data.container);
+                $.bootstrapGrowl('Volume activated.', {type: 'success'});
+            })
+            .error(function (xhr, ajaxOptions, thrownError) {
+                $.bootstrapGrowl('Oops, something went wrong!', {type: 'info'});
+            });
+        });
+        
+        // deactivate
+        tr.find('a#deactivate').click(function(e) {
+            // update container
+            var _container = {
+                id: container_.id,
+                active: false,
+            };
+            
+            $.ajax({
+                type: 'POST',
+                url: '/container/update',
+                contentType: 'application/json;charset=utf-8',
+                dataType: 'json',
+                data: JSON.stringify({
+                    container: _container,
+                }),
+            })
+            .done(function(data) {
+                // required to fix variable "container" from closure
+                _.each(_container, function(value, key) { container_[key] = value; });
+                
+                // update UI
+                container._update(data.container);
+                $.bootstrapGrowl('Volume deactivated.', {type: 'success'});
+            })
+            .error(function (xhr, ajaxOptions, thrownError) {
+                $.bootstrapGrowl('Oops, something went wrong!', {type: 'info'});
+            });
+        });
+        
+        // remove
+        tr.find('a#remove').click(function(e) {
+            $.ajax({
+                type: 'POST',
+                url: '/container/remove',
+                contentType: 'application/json;charset=utf-8',
+                dataType: 'json',
+                data: JSON.stringify({
+                    id: container_.id,
+                }),
+            })
+            .done(function(data) {
+                tr.remove();
+                $.bootstrapGrowl('User successfully removed.', {type: 'success'});
+            })
+            .error(function (xhr, ajaxOptions, thrownError) {
+                $.bootstrapGrowl('Oops, something went wrong!', {type: 'info'});
+            });
+        });
+    },
+    
+    add: function(options) {
+        options = options || {};
+        var new_template = _.template($('#modal-new-container').html());
+        var modal_div = $(new_template());
+        var host_id_select = modal_div.find('#host_id');
+        var mount_point_id_select = modal_div.find('#mount_point_id');
+        
+        // populate hosts
+        $.ajax({
+            type: 'POST',
+            url: '/hosts/all',
+            contentType: 'application/json;charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify({}),
+        })
+        .done(function(data) {
+            // console.log(data);
+            _.each(data.hosts, function(host_) {
+                var option = $('<option>')
+                    .attr('value', host_.id)
+                    .text(host_.name)
+                    .appendTo(host_id_select);
+            });
+        })
+        .error(function (xhr, ajaxOptions, thrownError) {
+            $.bootstrapGrowl('Oops, something went wrong!', {type: 'info'});
+        });
+        
+        // populate mount points
+        $.ajax({
+            type: 'POST',
+            url: '/mount/points/all',
+            contentType: 'application/json;charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify({}),
+        })
+        .done(function(data) {
+            // console.log(data);
+            _.each(data.mounts, function(mount_point_) {
+                var option = $('<option>')
+                    .attr('value', mount_point_.id)
+                    .text(mount_point_.name)
+                    .appendTo(mount_point_id_select);
+            });
+        })
+        .error(function (xhr, ajaxOptions, thrownError) {
+            $.bootstrapGrowl('Oops, something went wrong!', {type: 'info'});
+        });
+        
+        // close
+        modal_div.find('button#close').click(function(e) {
+            // close modal
+            modal_div.modal('hide');
+            setTimeout(function() { modal_div.remove();}, 500);
+            $('.modal-backdrop').remove();
+        });
+        
+        // update
+        modal_div.find('button#create').click(function(e) {
+            // close modal
+            modal_div.modal('hide');
+            setTimeout(function() { modal_div.remove();}, 500);
+            $('.modal-backdrop').remove();
+            
+            // create container
+            var _container = {
+                name: modal_div.find('#name').val(),
+                capacity: modal_div.find('#capacity').val(),
+                host_id: modal_div.find('#host_id').val(),
+                mount_point_id: modal_div.find('#mount_point_id').val(),
+                username: modal_div.find('#username').val(),
+            };
+            
+            $.ajax({
+                type: 'POST',
+                url: '/container/create',
+                contentType: 'application/json;charset=utf-8',
+                dataType: 'json',
+                data: JSON.stringify({
+                    container: _container,
+                }),
+            })
+            .done(function(data) {
+                container._add(data.container);
+                $.bootstrapGrowl('Volume successfully created.', {type: 'success'});
+            })
+            .error(function (xhr, ajaxOptions, thrownError) {
+                $.bootstrapGrowl('Oops, something went wrong!', {type: 'info'});
+            });
+        });
+        
+        modal_div.modal({
+            backdrop: 'static',
+        });
+    },
+    
+    _update: function(container_) {
+        var tr = $('tr[data-id="' + container_.id + '"]');
+        
+        _.each(container_, function(value, key) {
+            if (key === 'id') return;
+            var td = tr.find('td#' + key);
+            td.text(value);
+        });
+    },
+});
+
